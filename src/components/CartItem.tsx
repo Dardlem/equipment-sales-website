ad8953e1c583502f372f2cdef9e157557b6ba40b
Dardlem <burmistrovartyom@gmail.com>
2023-01-11 14:21:06 +0200

    Firebase integration

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -22,7 +22,6 @@ function CartItem({id, quantity}: CartItemProps){
     });
 
     useEffect(() => {
-        console.log("CartItem useEffect")
         let e = fetchData();
 
         e.products.map((product) => {
@@ -42,19 +41,14 @@ function CartItem({id, quantity}: CartItemProps){
             <Grid container
                 alignContent="center"
                 justifyContent="center"
+                direction="row"
                 sx={{
                     padding: "10px 5px"
                 }}
             >
-                <Grid container direction="row" justifyContent="space-between"
-                    sx={{
-                        padding: "10px 0px"
-                    }}>
-                    <Typography variant="body2">{data.name}</Typography>
-                    <Typography variant="body2">Price: {data.price}</Typography>
-                </Grid>
-                <Grid container direction="row" justifyContent="space-between" alignContent="center">
-                    <Grid container direction="column" width="60%">
+                    <Typography marginBottom="10px" variant="h6">{data.name}</Typography>
+                <Grid container direction="row" justifyContent="space-between" alignItems="center" width="100%" flexWrap="nowrap">
+                    <Grid container direction="column" width="auto%" margin="10px">
                         <TextField
                             id="quantity-field"
                             label={"Quantity"}
@@ -67,19 +61,22 @@ function CartItem({id, quantity}: CartItemProps){
                                 width: '100%'
                             }}
                             onChange={(newValue) => { setItemQuantity(id, parseInt(newValue.target.value, 10)) }} />
-                            <Typography variant="body2">SubTotal: {DisplayCurrency(subTotal())}</Typography>
+                        <Typography variant="body2">SubTotal: {DisplayCurrency(subTotal())}</Typography>
                     </Grid>
-                    <Box component="img" src={`${data.preview}`}
-                        maxHeight="75px"borderRadius="25px" />
-                    <IconButton onClick={ () => {removeFromCart(id)} }
-                        sx={{
-                            width: '20px',
-                            height: '20px',
-                            alignSelf: 'center',
-                        }}
-                    >
-                        <Cancel />
-                    </IconButton>
+                    <Grid container direction="row" flexShrink="2" flexWrap="nowrap" alignItems="center">
+                        <Box component="img" src={`${data.preview}`}
+                            maxHeight="75px"borderRadius="25px" />
+                        <IconButton onClick={ () => {removeFromCart(id)} }
+                            sx={{
+                                width: '20px',
+                                height: '20px',
+                                alignSelf: 'center',
+                                margin: "5px"
+                            }}
+                        >
+                            <Cancel />
+                        </IconButton>
+                    </Grid>
                 </Grid>
             </Grid>
         </Card>
